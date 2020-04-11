77bb0a366a172913fdfaba63307141519373938d
as8811114 <43846907+as8811114@users.noreply.github.com>
2020-04-11 20:59:48 +0800

    20200411
    X你媽一堆bug

---
+++ Assets/Scripts/ARscene/CatController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CatController : MonoBehaviour
+{
+    float speed = 5.0f;
+    float y = 0.0f;
+    float direction = 0.0f;     //朝向前方
+    float timeOfDirection = 0;
+    float timeOfWalking = 0;
+    bool isOk = false;
+    Animator am;
+    // Start is called before the first frame update
+    void Start()
+    {
+        am = GetComponent<Animator>();
+        am.SetBool("isWalk", false);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeOfDirection += Time.deltaTime;
+        if (timeOfDirection >= 3.0f)
+        {
+            direction = Random.Range(0.0f,360.0f);
+            transform.eulerAngles = new Vector3(0.0f, direction, 0.0f);
+            isOk = true;
+            timeOfWalking = 1.5f;
+            timeOfDirection = 0.0f;
+        }
+        if(isOk && timeOfWalking > 0)
+        {
+            transform.position += transform.forward * Time.deltaTime;
+            am.SetBool("isWalk", true);
+            timeOfWalking -= Time.deltaTime;
+        }
+        else
+        {
+            am.SetBool("isWalk", false);
+        }
+
+    }
+
+}
---
+++ Assets/Scripts/ARscene/CreateFood.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateFood : MonoBehaviour
+{
+
+    public GameObject foodSource;
+    public GameObject parent;
+    GameObject food = null;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void createFood()
+    {
+
+        if (food == null)
+        {
+            food = Instantiate<GameObject>(foodSource);
+            food.transform.parent = parent.transform;
+            food.GetComponent<Transform>().localPosition = new Vector3(1.0f, 0.0f, 0.0f);
+            food.GetComponent<Transform>().localScale = new Vector3(0.015f, 0.015f, 0.015f);
+        }
+        
+    }
+}
