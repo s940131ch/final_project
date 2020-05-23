93c162082ea732198aa3e4eb92bf8db1eeddd592
Jay <s940131@gmail.com>
2020-05-23 22:34:29 +0800

    登入，操
    登入，操，fuck

---
+++ Assets/Scripts/MainMenu/LoginManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Assets;
+
+public class LoginManager : MonoBehaviour
+{
+
+    public InputField ID;
+    public InputField Password;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Signin()
+    {
+        Debug.Log(ID.text);
+        SqlAccess sql = new SqlAccess();
+        sql.Select(ID.text, Password.text);
+        sql.Close();
+    }
+
+    public void Signup()
+    {
+        Debug.Log(ID.text);
+        SqlAccess sql = new SqlAccess();
+        sql.Insert(ID.text, Password.text);
+        sql.Close();
+    }
+}
